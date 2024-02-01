c68f58cb43e78fd36c5d79975bd3c9db71bdbba6
Marta <martosm020@student.polsl.pl>
2024-02-01 23:26:51 +0100

    Cors config

--- EduManagmentSystem/src/main/java/com/example/EduManagmentSystem/controller/AccountController.java
+++ EduManagmentSystem/src/main/java/com/example/EduManagmentSystem/controller/AccountController.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.function.EntityResponse;
 
-@Controller
+@RestController
+@CrossOrigin(origins = "*")
 public class AccountController {
 
     @Autowired
