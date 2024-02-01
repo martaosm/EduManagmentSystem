a6711f3b54aef3e5a633549fc64cae060c73a6c1
szymon.kisiela <szymon.kisiela@asseco.pl>
2024-02-01 23:47:42 +0100

    version change

--- EduManagmentSystem/src/main/java/com/example/EduManagmentSystem/config/WebConfig.java
+++ EduManagmentSystem/src/main/java/com/example/EduManagmentSystem/config/WebConfig.java
@@ -5,15 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("http://pz-frontend.frontend.svc.cluster.local")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(false)
-                .maxAge(3600);
+                .allowCredentials(true);
     }
 }
