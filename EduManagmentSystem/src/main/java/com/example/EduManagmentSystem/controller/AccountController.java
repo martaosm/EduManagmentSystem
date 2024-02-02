42851c5ce4977e5fee9a5e2e4321617797399980
szymon.kisiela <szymon.kisiela@asseco.pl>
2024-02-02 12:59:53 +0100

    Rename service files to prepare for Prometheus monitoring
    Files for both frontend and backend services have been renamed to signal the readiness for Prometheus monitoring. This naming convention simplifies autodetection of the services by Prometheus, enabling automatic metrics collection.

--- EduManagmentSystem/src/main/java/com/example/EduManagmentSystem/controller/AccountController.java
+++ EduManagmentSystem/src/main/java/com/example/EduManagmentSystem/controller/AccountController.java
@@ -11,15 +11,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.function.EntityResponse;
 
+import io.opentracing.Tracer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @CrossOrigin(origins = "*")
 public class AccountController {
 
     @Autowired
-    AccountService accountService;
+    private AccountService accountService;
+
+    @Autowired
+    private Tracer tracer;
 
     @GetMapping("/accountDetails")
     public ResponseEntity<String> getAccountDetails(){
-        return ResponseEntity.ok(accountService.getAccountDetails().getUsername());
+        var span = tracer.buildSpan("getAccountDetails").start();
+        try {
+
+            String username = accountService.getAccountDetails().getUsername();
+            return ResponseEntity.ok(username);
+        } finally {
+            span.finish();
+        }
     }
 }
